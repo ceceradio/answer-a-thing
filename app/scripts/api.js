354eb39f087eb5b6e7e1e7a01d26ae02ae2478fb
Charlie <me@satsuki.tv>
2015-06-13 11:28:52 -0400

    sketching out the potential for a game state manager

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -3,4 +3,24 @@
 angular.module('answerAThingApp').
   factory('drawSocket', function (socketFactory) {
     return socketFactory({ ioSocket: io('http://devfluid.com:40001') });
+  })
+  .factory('gameState', function(drawSocket) {
+    var gameState = {
+      room: { name: "", password: "" },
+      user: { name: "", accessToken: "" },
+      state: 'loggedout'
+    }
+    drawSocket.on('user', function(user) {
+      if (user.room !== false) {
+        gameState.state = 'inroom';
+      }
+      else {
+        gameState.state = 'inlobby';
+      }
+    });
+    drawSocket.on('logout', function(error) {
+      console.log(error);
+      gameState.state = 'loggedout';
+    });
+    return gameState;
   });
