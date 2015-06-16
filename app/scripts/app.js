85be88e10577fbc22e7399ba0f67ffe1875f331c
Charlie <me@satsuki.tv>
2015-06-15 23:10:54 -0400

    sketching out the routing for the app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,11 +17,23 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
+      })
+      .when('/lobby', {
+        templateUrl: 'views/lobby.html',
+        controller: 'LobbyController'
+      })
+      .when('/createroom', {
+        templateUrl: 'views/createroom.html',
+        controller: 'CreateRoomController'
+      })
+      .when('/room/:room_id', {
+        templateUrl: 'views/room.html',
+        controller: 'RoomController'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/login'
       });
   });
