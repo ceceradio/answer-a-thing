e0a405d7760c8465f250edeb357df9cf2ca79a74
Charlie <me@satsuki.tv>
2015-06-17 20:22:20 -0400

    more restructuring. moved login/user caching code into the api.js file and its own controller

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -4,11 +4,17 @@ angular.module('answerAThingApp').
   factory('drawSocket', function (socketFactory) {
     return socketFactory({ ioSocket: io('http://devfluid.com:40001') });
   })
-  .factory('gameState', function(drawSocket, $location) {
+  .factory('gameState', function(drawSocket, $location, $window) {
     var gameState = {
       room: { name: "", password: "" },
-      user: { name: "", accessToken: "" },
+      user: { username: "", accessToken: "" },
       state: 'loggedout'
+    }
+    gameState.user.username = $window.localStorage.getItem('username');
+    gameState.user.accessToken = $window.localStorage.getItem('accessToken');
+    if (!gameState.user.accessToken) {
+      gameState.user.accessToken = CryptoJS.SHA3(CryptoJS.lib.WordArray.random(1024)).toString(CryptoJS.enc.Base64);
+      $window.localStorage.setItem('accessToken', gameState.user.accessToken);
     }
     drawSocket.on('user', function(user) {
       if (user.room !== false) {
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name answerAThingApp.controller:LoginController
+ * @description
+ * # LoginController
+ * Controller of the answerAThingApp
+ */
+angular.module('answerAThingApp')
+  .controller('LoginController', function ($scope, drawSocket, gameState) {
+    $scope.user = gameState.user;
+    $scope.error = false;
+    $scope.login = function() {
+      window.localStorage.setItem('username', $scope.user.username);
+      drawSocket.emit('login', $scope.user);
+    };
+    drawSocket.on('logout', function(error) {
+      $scope.error = error;
+    });
+    if ($scope.user.username) {
+      $scope.login();
+    }
+  });
