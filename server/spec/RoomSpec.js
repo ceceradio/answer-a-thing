4ee651b022b44bd1c9a89e7ae6f2e416abb601c2
Charlie <me@satsuki.tv>
2015-08-17 19:03:46 -0400

    added a spec for Room.resetBets()

--- server/spec/RoomSpec.js
+++ server/spec/RoomSpec.js
@@ -1,3 +1,21 @@
+var User = require('../User.js');
+var Room = require('../Room.js');
+var rooms;
 describe("Room", function() {
-  
+    var room;
+    beforeEach(function() {
+      room = new Room();
+      // clear the rooms
+      rooms = Room.getRooms();
+      for (var member in rooms) delete rooms[member];
+    });
+    describe('.resetBets()', function() {
+      it("should set all user's bets property to false", function() {
+        room.users = [{ bets: 1 }, { bets: 2 }];
+        room.resetBets();
+        for(var i = 0; i < room.users.length; i++) {
+          expect(room.users[i].bets).toEqual(false);
+        }
+      });
+    });
 });
